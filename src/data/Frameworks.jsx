f0c0c0c696d86db4770364c20b254196f45cbe40
Luis Álvarez <luigi>
2025-08-28 23:45:17 -0400

    cambios en los frameworks

--- src/data/Frameworks.jsx
+++ src/data/Frameworks.jsx
@@ -2,26 +2,24 @@ import {
   SiBootstrap,
   SiMui,
   SiTailwindcss,
-  SiFramer,
   SiReact,
   SiDotnet,
   SiPandas,
   SiDjango,
   SiNodedotjs,
-  SiNestjs
+  SiVite
 } from 'react-icons/si';
 
 const frameworks = [
     { name: 'Bootstrap', icon: <SiBootstrap size={50} className="skill-icon" /> },
     { name: 'React', icon: <SiReact size={50} className="skill-icon" /> },
+    { name: 'Vite', icon: <SiVite size={50} className="skill-icon" /> },
     { name: 'Material UI', icon: <SiMui size={50} className="skill-icon" /> },
     { name: 'Tailwind', icon: <SiTailwindcss size={50} className="skill-icon" /> },
-    { name: 'Framer Motion', icon: <SiFramer size={50} className="skill-icon" /> },
     { name: '.NET', icon: <SiDotnet size={50}  className="skill-icon" /> },
     { name: 'Django', icon: <SiDjango size={50} className="skill-icon" /> },
     { name: 'Pandas', icon: <SiPandas size={50} className="skill-icon" /> },
     { name: 'Node.js', icon: <SiNodedotjs size={50} className="skill-icon" /> },
-    { name: 'Nest.js', icon: <SiNestjs size={50} className="skill-icon" /> }
 ];
 
 export default frameworks;
